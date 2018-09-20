5040f7271cf109c2253799c44afef1e5beefbe2c
myles <myles@Myles-Laptop.MilkyWay.com>
2018-09-20 10:21:41 -0500

    Starting Config parsing

--- src/main/java/riggbot/config/Config.java
+++ src/main/java/riggbot/config/Config.java
@@ -2,12 +2,14 @@ package riggbot.config;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import riggbot.logger.ErrorCodes;
 import riggbot.logger.Logger;
 
 public class Config {
-
+	
+	private static HashMap<String, ConfigValue> cfgValues = new HashMap<>();
 	public static final File cfg = new File("config/config.cfg");
 
 	public static boolean configExist() {
@@ -21,16 +23,20 @@ public class Config {
 			populateCfg(cfg);
 			return false;
 		} else {
-			
+			parseConfig(cfg);
 			return true;
 		}
 	}
 
+	private static void parseConfig(File file) {
+		
+	}
+
 	private static void populateCfg(File file) {
-		ConfigWriter.makeHeader(cfg);
-		ConfigWriter.write(cfg, new ConfigValue(ConfigTypes.STRING, "testvalString", "haha lol", "defaultString", "lol zozole test desc"));
-		ConfigWriter.write(cfg, new ConfigValue(ConfigTypes.INT, "testvalnt", "69696969", "defaultInt", "lol zozole test desc"));
-		ConfigWriter.write(cfg, new ConfigValue(ConfigTypes.BOOLEAN, "testvalBoolean", "false", "defaultBool", "lol zozole test desc"));
-		ConfigWriter.write(cfg, new ConfigValue(ConfigTypes.DOUBLE, "testvalDouble", "420.69", "defaultDouble", "lol zozole test desc"));
+		ConfigWriter.makeHeader(file);
+		ConfigWriter.write(file, new ConfigValue(ConfigTypes.STRING, "testvalString", "haha lol", "defaultString", "lol zozole test desc"));
+		ConfigWriter.write(file, new ConfigValue(ConfigTypes.INT, "testvalnt", "69696969", "defaultInt", "lol zozole test desc"));
+		ConfigWriter.write(file, new ConfigValue(ConfigTypes.BOOLEAN, "testvalBoolean", "false", "defaultBool", "lol zozole test desc"));
+		ConfigWriter.write(file, new ConfigValue(ConfigTypes.DOUBLE, "testvalDouble", "420.69", "defaultDouble", "lol zozole test desc"));
 	}
 }
