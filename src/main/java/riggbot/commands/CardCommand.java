01dcbe37b871df9842bbd93e127325f842e2f2b8
Riggs78 <dcjeske@usa.net>
2018-09-22 20:23:03 -0500

    changed naming of cards, added more

--- src/main/java/riggbot/commands/CardCommand.java
+++ src/main/java/riggbot/commands/CardCommand.java
@@ -15,6 +15,7 @@ public class CardCommand extends Command {
 
 	public CardCommand() {
 		this.name = "card";
+		this.aliases = new String[] { "draw" , "c" };
 		this.botPermissions = new Permission[] { Permission.MESSAGE_WRITE };
 		this.help = "Picks a card, any card";
 	}
@@ -23,11 +24,25 @@ public class CardCommand extends Command {
 	protected void execute(CommandEvent event) {
 		TextChannel chan = event.getTextChannel();
 		Message msg = event.getMessage();
+		// 109 jpgs, 106 pngs
+		int fti = (int) Math.floor(Math.random() * 2);
+		String fts = ".img";
 		int num = 0;
-		num = (int) Math.floor(Math.random() * 69 + 1);
+		Logger.logDebug("card ("+num+")"+fts);
+		if (fti == 0) {
+			fts = ".png";
+			num = (int) Math.floor(Math.random() * 106 + 1);
+			Logger.logDebug("card ("+num+")"+fts);
+		} else {
+			fts = ".jpg";
+			num = (int) Math.floor(Math.random() * 109 + 1);
+			Logger.logDebug("card ("+num+")"+fts);
+		}
 		final int Num = num;
-		chan.sendFile(new File("src/main/resources/Cards/"+num+".png")).queue(m -> {
-			Logger.logInfo(CommandUtil.getName(msg) + " ran command \"card\", received " + Num);
+		final String FTS = fts;
+		Logger.logDebug("card ("+num+")"+fts);
+		chan.sendFile(new File("src/main/resources/Cards/card (" + num +")"+ fts)).queue(m -> {
+			Logger.logInfo(CommandUtil.getName(msg) + " ran command \"card\", received " + "card ("+Num+")"+FTS);
 		});
 	}
 }
